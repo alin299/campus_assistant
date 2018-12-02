3680307eed35dd4cbec8a99d439911570abe9284
alin <823301694@qq.com>
2018-12-02 13:33:52 +0800

    ·从阿里云上部署的docker容器中获取课程表

--- pages/timetable/timetable.js
+++ pages/timetable/timetable.js
@@ -5,32 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-    colors: ['#98FB98', '#D8BFD8', '#DB7093', '#B0C4DE', '#87CEFA', '#ADD8E6', '#E6E6FA', '#FF7F50', '#DCDCDC','#6495ED'],
-    courses: [
-      { start: 1, step: 2, day: 1, name: "计算机组成" },
-      { start: 5, step: 2, day: 1, name: "操作系统" },
-      { start: 3, step: 2, day: 2, name: "移动应用开发" },
-      { start: 5, step: 2, day: 2, name: "单片机原理及应用" },
-      { start: 3, step: 2, day: 3, name: "单片机原理及应用" },
-      { start: 5, step: 2, day: 3, name: "计算机组成" },
-      { start: 3, step: 2, day: 4, name: "移动应用开发" },
-      { start: 3, step: 2, day: 5, name: "数据库实验" },
-      { start: 5, step: 2, day: 5, name: "操作系统" }
-    ]
+    colors: ['#FFFACD', '#D8BFD8', '#DB7093', '#B0C4DE', '#87CEFA', '#ADD8E6', '#E6E6FA', '#FF7F50', '#DCDCDC','#6495ED'],
+  //   courses: [
+  //     { start: 1, step: 2, day: 1, name: "计算机组成@12-505" },
+  //     { start: 5, step: 2, day: 1, name: "操作系统@32-307" },
+  //     { start: 3, step: 2, day: 2, name: "移动应用开发@31-402" },
+  //     { start: 5, step: 2, day: 2, name: "单片机实验@31" },
+  //     { start: 1, step: 2, day: 3, name: "单片机原理及应用@33-B306" },
+  //     { start: 3, step: 2, day: 3, name: "计算机组成@32-502" },
+  //     { start: 1, step: 2, day: 4, name: "移动应用开发@31-402" },
+  //     { start: 1, step: 2, day: 5, name: "数据库实验@31-402" },
+  //     { start: 3, step: 2, day: 5, name: "操作系统@8-101" }
+  //   ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCourses();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
@@ -73,5 +73,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getCourses:function(){
+    var that = this;
+    wx.request({
+      url: 'http://www.alin299.top:5000/courses', 
+      data: {
+        
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success(res) {
+        console.log(res.data.courses)
+        that.setData({
+          'courses': res.data.courses
+        })
+      }
+    })
   }
 })
