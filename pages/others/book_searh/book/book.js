6e12b2bff66d99a08f013107284a1ac54d1af325
alin <823301694@qq.com>
2018-12-30 22:59:48 +0800

    添加图书详情与评论界面

---
+++ pages/others/book_searh/book/book.js
@@ -0,0 +1,70 @@
+// pages/others/book_searh/book/book.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    item: { cover: "https://img3.doubanio.com/view/subject/l/public/s29902192.jpg", name:'冬泳',
+      author: '班宇', ISBN: 9787542664051, publishing: '理想国丨上海三联书店'},
+    comments: [{ id: 1, name: 'alin', content: '这篇小说是典型的悲剧结构。人遭遇命运的刁难，解脱的途径就是自我毁灭。', mark: 4, avatar: 'https://img3.doubanio.com/icon/u2189889-43.jpg'},
+      { id: 1, name: 'alin', content: '第一次读班宇，是在《西湖》杂志上，那篇叫《铁西夜曲》的小说，不同于这本刚出来的《冬泳》，凌厉而不失花样的语言直指漫长无望的现实。', mark: 5, avatar: 'https://img3.doubanio.com/icon/u1075046-2.jpg' },
+      { id: 1, name: 'alin', content: '这是众多傍晚中的一个，并不比昨天或明天的更为独特，但却也同样的晦暗、易逝、难以捕捉。', mark: 4, avatar: 'https://img3.doubanio.com/icon/u156203975-1.jpg' }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
